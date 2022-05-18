01119b60ce8be22c4d5ba763b0d54a9d76108053
Olya Belalova <oybelalova@gmail.com>
2022-05-18 20:35:21 +0300

    new table

--- src/components/blogs.jsx
+++ src/components/blogs.jsx
@@ -6,9 +6,24 @@ const blogs = () => {
         <table class="table">
             <tbody>
                 <tr>
-                    {blogPosts.map((blog) => (
-                        <td>{blog.title}</td>
-                    ))}
+                    {blogPosts.map((blog) =>
+                        blog.id < 4 ? <td>{blog.title}</td> : ""
+                    )}
+                </tr>
+                <tr>
+                    {blogPosts.map((blog) =>
+                        blog.id < 4 ? <td>{blog.body}</td> : ""
+                    )}
+                </tr>
+                <tr>
+                    {blogPosts.map((blog) =>
+                        blog.id > 3 ? <td>{blog.title}</td> : ""
+                    )}
+                </tr>
+                <tr>
+                    {blogPosts.map((blog) =>
+                        blog.id > 3 ? <td>{blog.body}</td> : ""
+                    )}
                 </tr>
             </tbody>
         </table>
